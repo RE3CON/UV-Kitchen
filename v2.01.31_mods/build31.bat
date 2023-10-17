d120253e1ab4aaa4683b273c189db52ddf2e4682
Chris <write2recon@gmail.com>
2023-10-17 17:18:57 +0200

    Optimization and enhancements

--- v2.01.31_mods/build31.bat
+++ v2.01.31_mods/build31.bat
@@ -12,7 +12,7 @@ rem python v31\mod_custom_bootscreen_narrow.py temp\fw.dec.bin
 python v31\mod_more_freq_steps_and_backlight_duration.py temp\fw.dec.bin
 
 :: start of mods
-REM python mod_051f_ramreader.py temp\fw.dec.bin
+REM python v31\mod_051f_ramreader.py temp\fw.dec.bin
 python v31\mod_2to1_compressor.py temp\fw.dec.bin
 REM python v31\mod_battery_icon.py temp\fw.dec.bin
 python v31\mod_change_burst_tones.py temp\fw.dec.bin
@@ -20,8 +20,10 @@ python v31\mod_change_contrast.py temp\fw.dec.bin
 python v31\mod_change_freq_scan_timeout.py temp\fw.dec.bin
 python v31\mod_change_RF_switch_threshold.py temp\fw.dec.bin
 python v31\mod_change_tx_limits.py temp\fw.dec.bin
+rem python v31\python mod_remove_tx_limits.py temp\fw.dec.bin
 python v31\mod_custom_ctcss_codes.py temp\fw.dec.bin
-
+rem python v31\mod_custom_font_VCR.py	temp\fw.dec.bin
+rem python v31\mod_custom_font_DO7OO.py	temp\fw.dec.bin
 python v31\mod_custom_digits.py temp\fw.dec.bin
 python v31\mod_custom_font.py temp\fw.dec.bin
 python v31\mod_custom_mdc_data.py temp\fw.dec.bin
@@ -38,9 +40,12 @@ python v31\mod_enable_swd_port.py temp\fw.dec.bin
 REM python v31\mod_enable_tx_50to850.py temp\fw.dec.bin
 python v31\mod_enable_tx_50to850_except_limits.py temp\fw.dec.bin
 python v31\mod_fm_radio_64-108mhz.py temp\fw.dec.bin
+REM v31\python mod_fm_radio_88-108mhz.py temp\fw.dec.bin
+REM v31\python mod_fm_radio_87-108mhz.py temp\fw.dec.bin
 python v31\mod_increase_backlight_timeout.py temp\fw.dec.bin
 REM python v31\mod_instant_on.py temp\fw.dec.bin
 python v31\mod_menu_strings.py temp\fw.dec.bin
+rem v31\python mod_menu_strings_org.py	temp\fw.dec.bin
 python v31\mod_mic_gain.py temp\fw.dec.bin
 REM python v31\mod_negative_screen.py temp\fw.dec.bin
 rem python v31\mod_ota_qrg.py temp\fw.dec.bin
